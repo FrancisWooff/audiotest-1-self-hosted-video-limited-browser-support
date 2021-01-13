41f2ea113c38b9d5f0d79f6e7f5c87232519d11e
FrancisW <65406277+franciswooff@users.noreply.github.com>
2021-01-13 10:31:44 +0000

    Update results2mailp2.php
    tidying & inclusion of instructions comment

--- results2mailp2.php
+++ results2mailp2.php
@@ -14,14 +14,17 @@ $name=$_POST[("name")];
 $lvla=$_POST[("ares")];
 $lvlb=$_POST[("bres")];
 $lvlc=$_POST[("cres")];
+
+//For each additional channel on your html test page you will need an additional line above with the appropriate letter ("dres" etc). You will then need to edit line 20 (beginning "echo") to add "<p>Slider D ".$lvld."</p>" etc. & line 22 (beginning "mail") to add "." Slider D".$lvld" etc.
+
 $mail="you@edu.salford.ac.uk";
 
-echo "<p>Thanks ".$name."</p><p>Your results have been sent to name</p><p>Slider A ".$lvla."</p><p>Slider B ".$lvlb."</p><p>Slider C ".$lvlc."</p>";
+echo "<p>Thanks ".$name."</p><p>Your results have been sent</p><p>Slider A ".$lvla."</p><p>Slider B ".$lvlb."</p><p>Slider C ".$lvlc."</p>";
 
-mail($mail,"Audio Test Results p2",$name." ".$lvla." ".$lvlb." ".$lvlc);
+mail($mail,"Audio Test Results P2 ".$name,"Slider A ".$lvla." Slider B ".$lvlb." Slider C".$lvlc);
 ?>
 
-<a href="index.html">Restart test</a>
+<a href="index.html">Return to start of test</a>
 
 </main>
 </body>
