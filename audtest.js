744665df4a001936c75724eeddbc288fe557463c
FrancisW <65406277+franciswooff@users.noreply.github.com>
2021-01-13 10:21:35 +0000

    Update audtest.js
    JS updated with "tighter" functions & ES6 variables

--- audtest.js
+++ audtest.js
@@ -1,34 +1,32 @@
-function abut() {
-    video.audioTracks[0].enabled = true;
-    video.audioTracks[1].enabled = false;
-    video.audioTracks[2].enabled = false;
-    
-}
-function bbut() {
-    video.audioTracks[0].enabled = false;
-    video.audioTracks[1].enabled = true;
-    video.audioTracks[2].enabled = false;
-    
-}
-function cbut() {
-    video.audioTracks[0].enabled = false;
-    video.audioTracks[1].enabled = false;
-    video.audioTracks[2].enabled = true;
-    
+const but = document.querySelectorAll("button");
+const vid = document.querySelector("video");
+const trx = vid.audioTracks;
+console.log(vid);
+console.log(trx);
+
+but.forEach(prsd);
+function prsd(item, index) {
+  let x = index;
+
+  item.addEventListener("click",function(){    
+    let i;
+    for (i = 0; i < but.length; i++){
+      but[i].style.backgroundColor = "grey";
+      trx[i].enabled = false;
+    }
+
+    but[x].style.backgroundColor = "white";
+    trx[x].enabled = true;
+    })
 }
 
-var slida = document.getElementById("aslid");
-var opa = document.getElementById("la");
-slida.oninput = function() {
-  opa.value = this.value;
-}
-var slidb = document.getElementById("bslid");
-var opb = document.getElementById("lb");
-slidb.oninput = function() {
-  opb.value = this.value;
-}
-var slidc = document.getElementById("cslid");
-var opc = document.getElementById("lc");
-slidc.oninput = function() {
-  opc.value = this.value;
+const slid = document.querySelectorAll("[type=range]");
+const ipnm = document.querySelectorAll("[type=number]");
+
+slid.forEach(slide);
+function slide(item, index) {
+  item.addEventListener("input",function(){
+    let i = index;
+    ipnm[i].value = this.value;
+  })
 }
